e27af3f615118733431921e7d11ef95cd4fd5ede
Oliver <1225191678@qq.com>
2017-09-05 18:39:41 +0800

    增加代理

--- spider/ms_utils/common.py
+++ spider/ms_utils/common.py
@@ -9,7 +9,7 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from ms_constants import WEB_DRIVER_CHROME, WEB_DRIVER_PHANTOMJS, PHANTOMJS, \
-    CHROME, HEADER_CHROME_1, HEADERS
+    CHROME, HEADER_CHROME_1, HEADERS, PROXY_HOST, PROXY_PORT, HEADER_FF_1, HEADER_IE_1
 from ms_exceptions import *
 from ms_utils import log
 
@@ -20,6 +20,14 @@ DEFAULT_TIMEOUT = 180
 
 
 def get_html_content(url, url_log=True):
+    if isinstance(PROXY_HOST, str) and isinstance(PROXY_PORT, int):
+        proxy_info = {'host': PROXY_HOST, 'port': PROXY_PORT}
+        print proxy_info
+        proxy_support = urllib2.ProxyHandler(
+            {'http': 'http://%(host)s:%(port)d' % proxy_info})
+        print {'http': 'http://%(host)s:%(port)d' % proxy_info}
+        opener = urllib2.build_opener(proxy_support)
+        urllib2.install_opener(opener)
     url = str(url).strip()
     timer = 1
     while True:
@@ -56,6 +64,7 @@ def get_html_content(url, url_log=True):
 
     # for special case
     if len(content) < 500 and 'loldytt' in url:
+        print content
         res = re.findall(r'window.location=(.*?)[;|<]', content, re.S)
         if len(res) == 1:
             location = 'http://www.loldytt.com' + res[0].replace('"',
@@ -66,7 +75,7 @@ def get_html_content(url, url_log=True):
     return content
 
 
-def get_webdriver(set_headers=False, disable_load_image=False):
+def get_webdriver(set_headers=False, disable_load_image=False, set_proxy=False):
     driver = None
     if PHANTOMJS or CHROME:
         try:
@@ -76,10 +85,25 @@ def get_webdriver(set_headers=False, disable_load_image=False):
                     random.choice(HEADERS))
                 if disable_load_image:
                     dcap["phantomjs.page.settings.loadImages"] = False
-                driver = webdriver.PhantomJS(WEB_DRIVER_PHANTOMJS)
+                if set_proxy:
+                    proxy_info = {'host': PROXY_HOST, 'port': PROXY_PORT}
+                    service_args = [
+                        '--proxy=%(host)s:%(port)d' % proxy_info,
+                        '--proxy-type=http',
+                    ]
+                    # proxy = webdriver.Proxy()
+                    # proxy.proxy_type = ProxyType.MANUAL
+                    # proxy.http_proxy = random.choice(ips)
+                    # proxy.add_to_capabilities(dcap)
+                else:
+                    service_args = None
+                driver = webdriver.PhantomJS(WEB_DRIVER_PHANTOMJS,
+                                             desired_capabilities=dcap,
+                                             service_args=service_args)
+                driver.set_page_load_timeout(DEFAULT_TIMEOUT)
             elif CHROME:
                 driver = webdriver.Chrome(WEB_DRIVER_CHROME)
-            driver.set_page_load_timeout(DEFAULT_TIMEOUT)
+                # TODO: not finished for chrome
         except Exception as e:
             raise e
         else:
@@ -107,20 +131,22 @@ def format_lol_name(name):
 
 
 if __name__ == '__main__':
+    import requests
 
-    def get_lol_index():
-        driver = None
-        try:
-            driver = get_webdriver(disable_load_image=True, set_headers=True)
-            driver.get('http://www.loldytt.com')
-        except WebDriverTimeOut:
-            pass
-        except Exception as e:
-            if driver:
-                driver.quit()
-            raise e
-        else:
-            driver.quit()
-            pass
+    # s = requests.Session()
+    # r = s.get('http://www.baidu.com',
+    #           headers={
+    #               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+    #           '(KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    #               'Connection': 'keep-alive'
+    #           },
+    #           proxies={'http': 'http://116.196.101.221:8088'})
+    # print r.content
 
-    get_lol_index()
+    r = get_html_content('http://www.loldytt.com', url_log=False)
+    print r
+
+    # driver = get_webdriver(set_headers=True, disable_load_image=True, set_proxy=True)
+    # print driver
+    # driver.get('http://www.loldytt.com')
+    # print driver.page_source
