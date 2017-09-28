88136d68d293a11319b3b0ea131683d9c6dde253
Oliver <1225191678@qq.com>
2017-09-28 22:38:42 +0800

    modify spider

--- spider_client/scheduler.py
+++ spider_client/scheduler.py
@@ -4,7 +4,7 @@ import time
 from ms_main import Main
 
 INTERVAL = 60
-START_POINT = (10, 18, 22)
+START_POINT = (10, 14, 18, 22)
 
 
 def get_hour():
@@ -18,31 +18,36 @@ def get_datetime():
 class Schedule(object):
 
     def __init__(self):
-        self.last_point = None  # last run time
+        self.last_hour = None  # last run time
         self.first = True   # when run schedule
         self.printed = False
+        self.next_hour = get_hour()
+
+    @staticmethod
+    def _get_next_hour():
+        now_hour = get_hour()
+        if max(START_POINT) > now_hour:
+            return (now_hour + min([item - now_hour for item in START_POINT
+                                    if item > now_hour]))
+        else:
+            return min(START_POINT)
 
     def run(self):
         while True:
             hour_now = get_hour()
 
-            if (hour_now in START_POINT and hour_now != self.last_point) or self.first:
-                if self.first:
-                    self.first = False
+            if self.next_hour == hour_now:
+                self.first = False
                 print 'Schedule started at %s' % get_datetime()
                 Main().start()
-                self.last_point = hour_now
                 self.printed = False
+                self.next_hour = self._get_next_hour()
                 print 'Schedule ended at %s' % get_datetime()
             else:
-                try:
-                    next_hour = (hour_now +
-                                 min([item - hour_now for item in START_POINT if item > hour_now]))
-                    if not self.printed:
-                        print 'Schedule will run at %.2d:00' % next_hour
-                        self.printed = True
-                except ValueError:
-                    pass
+                if not self.printed:
+                    print 'Next schedule will run at %.2d:00' % self.next_hour
+                    self.printed = True
+
                 time.sleep(INTERVAL)
 
 
